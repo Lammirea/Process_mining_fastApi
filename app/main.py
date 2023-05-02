c77a100d8c909aee34362ef4294e95591b355651
Lammirea <ardalin.sun@mail.ru>
2023-05-02 21:28:48 +0300

    some huge change

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import json
 from app.minersFuncs import alpha_model
+import base64
 
 app = FastAPI()
 
@@ -12,5 +13,10 @@ templates = Jinja2Templates(directory="UI-template",auto_reload=True)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    model = alpha_model("repairExample.xes")
-    return templates.TemplateResponse("main.html", {"request": request , "data": model})
+    return templates.TemplateResponse("main.html", {"request": request})
+
+@app.post("/createModel")
+def upload(request: Request):
+    model = alpha_model("app/repairExample.xes")
+    base64_encoded_image = base64.b64encode(model).decode("utf-8")
+    return templates.TemplateResponse("main.html", {"request": request,  "PetriNet":base64_encoded_image})
--- app/minersFuncs.py
+++ app/minersFuncs.py
@@ -7,3 +7,4 @@ def alpha_model(filename):
     log = pm4py.read_xes(filename)
     net,initial_marking,final_marking = pm4py.discover_petri_net_alpha(log)
     return pm4py.view_petri_net(net,initial_marking,final_marking)
+
