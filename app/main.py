a9a94a9721eb3e4e4c53c0f46d91a70743a2c952
Lammirea <ardalin.sun@mail.ru>
2023-05-09 15:23:41 +0300

    Can built petri net from file xes

--- app/main.py
+++ app/main.py
@@ -16,18 +16,75 @@ templates = Jinja2Templates(directory="UI-template",auto_reload=True)
 async def home(request: Request):
     return templates.TemplateResponse("main.html", {"request": request})
 
-@app.post("/submit")
-def upload(request: Request, file: UploadFile = File(...)):
-    os.environ["PATH"] +=os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
+@app.post("/file/downloadAlpha")
+def upload_alpha(request: Request, file: UploadFile = File(...)):
     try:
         contents = file.file.read()
+        photo_path = os.path.join("buffer","alpha_"+file.filename)
+        with open(photo_path, "wb") as f:
+            f.write(contents)
 
+        model = alpha_model(photo_path)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
-    model = alpha_model(os.path.abspath(__file__))
+        os.remove(photo_path)
+    
     return templates.TemplateResponse("main.html", {"request": request,  "myImage": model})
+
+@app.post("/file/downloadInduct")
+def upload_inductive(request: Request, file: UploadFile):
+    try:
+        contents = file.file.read()
+        photo_path = os.path.join("buffer","inductive_"+file.filename)
+        with open(photo_path, "wb") as f:
+            f.write(contents)
+
+        modelInduct = inductive_model(photo_path)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+        os.remove(photo_path)
+    
+    return templates.TemplateResponse("main.html", {"request": request,  "myImage": modelInduct})
+
+@app.post("/file/downloadHeuristics")
+def upload_heuristics(request: Request, file: UploadFile):
+    try:
+        contents = file.file.read()
+        photo_path = os.path.join("buffer","heuristics_"+file.filename)
+        with open(photo_path, "wb") as f:
+            f.write(contents)
+
+        modelHeur = heuristics_model(photo_path)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+        os.remove(photo_path)
+    
+    return templates.TemplateResponse("main.html", {"request": request,  "myImage": modelHeur})
+
+@app.post("/file/downloadConformance")
+def upload_conformance(request: Request, file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        photo_path = os.path.join("buffer","conformance_"+file.filename)
+        with open(photo_path, "wb") as f:
+            f.write(contents)
+
+        modelConform = conformanceChecking(photo_path)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+        os.remove(photo_path)
+    
+    return templates.TemplateResponse("main.html", {"request": request,  "myImage": modelConform})
+
+
 
 # @app.get("/")
 # def main(request: Request):
@@ -46,16 +103,16 @@ def upload(request: Request, file: UploadFile = File(...)):
     
 #     model = alpha_model("app/repairExample.xes")
 #     return templates.TemplateResponse("index.html", {"request": request,  "myImage": model})
+# @app.post("/file/downloadAlpha")
+# def post_download_file():
+#     __file__ = "app/repairExample.xes"    
+#     return FileResponse(path=alpha_model(__file__), filename='petri_net_alpha.png', media_type='multipart/form-data')
 
-@app.get("/file/downloadAlpha")
-def download_file(file: UploadFile = File(...)):
-  __file__ = file
-  return FileResponse(path=alpha_model(os.path.abspath(__file__)), filename='petri_net_alpha.png', media_type='multipart/form-data')
-
-@app.get("/file/downloadInduct")
-def download_file():
-  __file__ = "app/repairExample.xes"
-  return FileResponse(path=inductive_model(os.path.abspath(__file__)), filename='petri_net_induct.png', media_type='multipart/form-data')
+# Загружает файл прямо на устройство
+# @app.get("/file/downloadInduct")
+# def download_file():
+#   __file__ = "app/repairExample.xes"
+#   return FileResponse(path=inductive_model(os.path.abspath(__file__)), filename='petri_net_induct.png', media_type='multipart/form-data')
 
 
 
--- app/minersFuncs.py
+++ app/minersFuncs.py
@@ -2,21 +2,40 @@ import os
 import pm4py
 from pm4py.algo.discovery.alpha import algorithm as alpha_miner
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
-
-
+from pm4py.visualization.heuristics_net import visualizer as hn_visualizer
+from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
+from pm4py.algo.discovery.heuristics import algorithm as heuristics_miner
+from pm4py.utils import get_properties
 
 def alpha_model(filename):
     os.environ["PATH"] +=os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
     logAlpha = pm4py.read_xes(filename)
     netAlpha, initial_markingAlpha, final_markingAlpha = alpha_miner.apply(logAlpha)
-    gviz = pn_visualizer.apply(netAlpha, initial_markingAlpha, final_markingAlpha)
-    # pn_visualizer.view(gviz)  
+    parameters = {pn_visualizer.Variants.FREQUENCY.value.Parameters.FORMAT: "png"}
+    gviz = pn_visualizer.apply(netAlpha, initial_markingAlpha, final_markingAlpha,parameters=parameters,
+            variant=pn_visualizer.Variants.FREQUENCY,log=logAlpha) 
     return pn_visualizer.view(gviz)
 
 def inductive_model(filename):
     os.environ["PATH"] +=os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
     logInduct = pm4py.read_xes(filename)
-    netInduct, initial_markingInduct, final_markingInduct = alpha_miner.apply(logInduct)
-    gvizInduct = pn_visualizer.apply(netInduct, initial_markingInduct, final_markingInduct)
+    parameters = {pn_visualizer.Variants.FREQUENCY.value.Parameters.FORMAT: "png"}
+    netInduct, initial_markingInduct, final_markingInduct = pm4py.discover_petri_net_inductive(logInduct)
+    gvizInduct = pn_visualizer.apply(netInduct, initial_markingInduct, final_markingInduct,parameters=parameters,
+                           variant=pn_visualizer.Variants.FREQUENCY,log=logInduct)
     return pn_visualizer.view(gvizInduct)
     
+def heuristics_model(filename):
+    os.environ["PATH"] +=os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
+    logHeur = pm4py.read_xes(filename)
+    heu_net = heuristics_miner.apply_heu(logHeur,parameters={heuristics_miner.Variants.CLASSIC.value.Parameters.DEPENDENCY_THRESH: 0.5})
+    gviz = hn_visualizer.apply(heu_net)
+    return hn_visualizer.view(gviz)
+
+def conformanceChecking(filename):
+    os.environ["PATH"] +=os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
+    log = pm4py.read_xes(filename)
+    netCheck, initial_markingCheck, final_markingCheck = pm4py.discover_petri_net_inductive(log)
+    replay_result = token_replay.apply(log, netCheck, initial_markingCheck, final_markingCheck)
+    gvizConform = pn_visualizer.apply(netCheck,parameters={pn_visualizer.Variants.FREQUENCY.value.Parameters.FORMAT: "png"})
+    return pn_visualizer.view(gvizConform)
